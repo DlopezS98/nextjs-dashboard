a1cc118e5f8f82f955304adccec12507f38a1f72
DlopezS98 <01dlopezs98@gmail.com>
2025-01-13 15:55:33 -0600

    rename LoginPage to SignupPage for clarity

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,8 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import LoginForm from '@/app/ui/login-form';
 import SignupForm from '../ui/signup-form';
 
-export default function LoginPage() {
+export default function SignupPage() {
     return (
         <main className="flex items-center justify-center md:h-screen">
             <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
